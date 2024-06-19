f5cac786d6f80ec6cfae39328bd706b30e2d9b08
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-06-20 02:19:18 +0530

    Auto commit

--- git_auto.py
+++ git_auto.py
@@ -14,26 +14,22 @@ def main():
         os.system("git init")
         os.system("touch .gitignore")
         with open(".gitignore", "w") as file:
-            file.write("*.pyc\n__pycache__\n.vscode\nmy-git.json\n")
-        if not os.path.exists(f"{path}/.git/my-git.json"):
-            with open(f"{path}/.git/my-git.json", "w") as file:
-                file.write(json.dumps({"online_repo": False}))
+            file.write("*.pyc\n__pycache__\n.vscode\nmy-git.json\n")        
         online_repo = args[1] if len(args) > 1 else None
         print(online_repo)
         if online_repo:
-            data = {}            
-            os.system(f"git remote add origin {online_repo}")
-            with open(f"{path}/.git/my-git.json", "r") as file:
-                data = json.loads(file.read())
-            data["online_repo"] = online_repo
+            os.system(f"git remote add origin {online_repo}")            
             with open(f"{path}/.git/my-git.json", "w") as file:
                 file.write(json.dumps(data))
         print("Git initialized")
     elif command == "save":
         cc = False
-        with open(f"{path}/.git/my-git.json", "r") as file:
-            data = json.loads(file.read())
-            cc = data["online_repo"]
+        try:
+            with open(f"{path}/.git/my-git.json", "r") as file:
+                data = json.loads(file.read())
+                cc = data["online_repo"]
+        except:
+            pass
         message = args[1] if len(args) > 1 else "Auto commit"
         os.system("git add .")
         os.system(f'git commit -m "{message}"')
