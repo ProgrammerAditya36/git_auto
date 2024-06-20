25426b3e15d8b90bc5373dabde72549c1677efb6
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-06-20 10:18:07 +0530

    Added features

--- git_auto.py
+++ git_auto.py
@@ -12,32 +12,35 @@ def main():
     command = args[0]
     if command == "init":
         os.system("git init")
-        os.system("touch .gitignore")
-        with open(".gitignore", "w") as file:
-            file.write("*.pyc\n__pycache__\n.vscode\nmy-git.json\n")        
+        os.system("touch .gitignore")             
         online_repo = args[1] if len(args) > 1 else None
         print(online_repo)
-        if online_repo:
-            os.system(f"git remote add origin {online_repo}")            
-            with open(f"{path}/.git/my-git.json", "w") as file:
-                file.write(json.dumps(data))
+        if online_repo:            
+            os.system(f"git remote add origin {online_repo}")
         print("Git initialized")
-    elif command == "save":
-        cc = False
-        try:
-            with open(f"{path}/.git/my-git.json", "r") as file:
-                data = json.loads(file.read())
-                cc = data["online_repo"]
-        except:
-            pass
+    elif command == "save":       
         message = args[1] if len(args) > 1 else "Auto commit"
         os.system("git add .")
-        os.system(f'git commit -m "{message}"')
-        if(cc):
+        os.system(f'git commit -m "{message}"')       
+        try:
+            os.system("git push -u origin main")
+        except Exception as e:
+            print("No online repository found")
+    elif command == "status":
+        os.system("git status")
+    elif command == "github":
+        online_repo = args[1] if len(args) > 1 else None
+        if online_repo:
             try:
-                os.system("git push -u origin main")
-            except:
-                print("Error in pushing online repo")
+                os.system(f"git remote add origin {online_repo}")
+            except Exception as e:
+                print("Repository already exists or invalid url")
+        else:
+            print("No online repository found")
+    elif command == "pull":
+        os.system("git pull origin main")
+    elif command == "log":
+        os.system("git log")
 
 
     
